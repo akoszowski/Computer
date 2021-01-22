30a81290b3ef45b5cabcd724cf947775b66bd9fa
lcaonmst <kzwierzchowski34@gmail.com>
2021-01-22 22:09:59 +0100

    Minor changes

--- computer.cc
+++ computer.cc
@@ -2,12 +2,12 @@
 #include "processor.h"
 #include "memory.h"
 
-Computer::Computer(mem_t size) : (*memory)(size), (*processor)() {}
+Computer::Computer(mem_t size) : memory(size), processor() {}
 
 void Computer::boot(program p) {
-	processor->execute(p);
+	processor.execute(p);
 }
 
 void Computer::memory_dump(std::ostream &os) {
-	memory->memory_dump(os);
+	memory.memory_dump(os);
 }
