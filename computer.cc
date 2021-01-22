12d022626013191228e961f8dc2c1b9a0d2bebc8
lcaonmst <kzwierzchowski34@gmail.com>
2021-01-22 22:04:47 +0100

    Added computer.cc

---
+++ computer.cc
@@ -0,0 +1,13 @@
+#include "computer.h"
+#include "processor.h"
+#include "memory.h"
+
+Computer::Computer(mem_t size) : (*memory)(size), (*processor)() {}
+
+void Computer::boot(program p) {
+	processor.execute(p);
+}
+
+void Computer::memory_dump(std::ostream &os) {
+	memory.memory_dump(os);
+}
