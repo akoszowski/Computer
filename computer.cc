60f6aa14935f4e89975ad6b01a2bb3149fce0e83
lcaonmst <kzwierzchowski34@gmail.com>
2021-01-22 22:07:09 +0100

    Minor changes

--- computer.cc
+++ computer.cc
@@ -5,9 +5,9 @@
 Computer::Computer(mem_t size) : (*memory)(size), (*processor)() {}
 
 void Computer::boot(program p) {
-	processor.execute(p);
+	processor->execute(p);
 }
 
 void Computer::memory_dump(std::ostream &os) {
-	memory.memory_dump(os);
+	memory->memory_dump(os);
 }
